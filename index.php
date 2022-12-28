9b70d386d1f008618761b9582b6e228de85fe42e
Doniyor Asadullaev <asadullaevdonier@gmail.com>
2022-12-28 20:27:00 +0500

    Directory iterator v1

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$iterator=new Class\UserIterator();
---
+++ src/Class/Iterator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Class;
+
+class UserIterator implements \Iterator
+{
+
+    public function __construct()
+    {
+        echo "works";
+    }
+    public function current()
+    {
+        // TODO: Implement current() method.
+    }
+    public function next()
+    {
+        // TODO: Implement next() method.
+    }
+    public function key()
+    {
+        // TODO: Implement key() method.
+    }
+    public function valid()
+    {
+        // TODO: Implement valid() method.
+    }
+    public function rewind()
+    {
+        // TODO: Implement rewind() method.
+    }
+}
